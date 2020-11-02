d095830bebdfdc47809aea2d5418341a7f4d5672
Oliver Lohmann <oliver@lohmann.io>
2020-11-02 14:56:16 +0100

    Preping new State Storage FW handling.

---
+++ tests/simple/01_tf/providers.tf
@@ -0,0 +1,4 @@
+provider "azurerm" {
+  version = "=2.34.0"
+  features {}
+}
--- tests/simple/01_tf/test.tf
+++ tests/simple/01_tf/test.tf
@@ -1,7 +1,3 @@
-provider "azurerm" {
-  version = "=1.44.0"
-}
-
 variable "resource_group_name" {
     type = string
 }
